0894b493969684d9fce4232c9bfd7742b3c801c9
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-05 19:29:42 +0530

    Time: 1 ms (30.43%), Space: 55.6 MB (47.1%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let sum = 0
+
+    for( i=1;i<=n;i++){
+         if( i % m !== 0){
+            sum += i
+         }else{
+            sum -= i
+         }
+    }
+
+    return sum
+};
