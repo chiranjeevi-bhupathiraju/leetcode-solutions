8bf293ee81d36120d9f5962c2d2e9497d267328c
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-07 10:17:23 +0530

    Time: 4 ms (3.1%), Space: 55.9 MB (7.74%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+   let capitalLength =  (word.match(/[A-Z]/g) || []).length;
+
+   if(capitalLength=== word.length || capitalLength === 0 || word[0].match(/[A-Z]/g)&& capitalLength === 1){
+    return true
+   }
+
+   console.log()
+
+   return false
+};
