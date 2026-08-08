8f0aff24469ec2415e4ed2742f1f5f14ff1edd10
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-08 15:11:11 +0530

    Time: 0 ms (100%), Space: 56.8 MB (29.08%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.js
@@ -7,8 +7,10 @@ var countPairs = function(nums, target) {
     let res = 0
 
     for(i=0;i<nums.length;i++){
+         let num1 = nums[i]
         for(j=i+1;j<nums.length;j++){
-             if(nums[i]+nums[j] < target){
+            let num2 = nums[j]
+             if(num1+num2 < target){
                 res++
              }
         }
