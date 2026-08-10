9d787225029b1f3d10aaef1358e48b8320716068
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-10 19:58:08 +0530

    Time: 3 ms (51.85%), Space: 55.9 MB (29.63%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.ts
@@ -0,0 +1,12 @@
+function countOperations(num1: number, num2: number): number {
+    let i= 0
+    while(num1 !== 0 && num2 !==0){
+        if(num1>=num2){
+            num1 = num1-num2
+        }else{
+            num2 = num2 - num1
+        }
+        i++
+    }
+    return i
+};
