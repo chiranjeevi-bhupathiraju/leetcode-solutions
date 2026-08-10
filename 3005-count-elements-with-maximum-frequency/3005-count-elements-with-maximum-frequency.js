93cfb5b48cb053a918bc79cad7f7262cc65ea225
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-10 10:03:14 +0530

    Time: 22 ms (5.56%), Space: 56.8 MB (46.53%) - LeetHub

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxFrequencyElements = function(nums) {
+    let freq = {}
+    let maxF = 0
+
+    for (i=0;i<nums.length;i++){
+        freq[nums[i]] = (freq[nums[i]] ?? 0)+1
+        maxF = Math.max(maxF,freq[nums[i]])
+    }
+
+    console.log(freq,maxF)
+
+    let s= 0
+    for(let e in freq){
+        if(freq[e] == maxF){
+            s += freq[e]
+        }
+    }
+
+    return s
+};
