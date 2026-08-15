c527e1b5455be16c78c1030f0ba1da8653679af8
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-15 06:43:28 +0530

    Time: 24 ms (29.41%), Space: 63.6 MB (27.45%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.ts
@@ -0,0 +1,8 @@
+function firstPalindrome(words: string[]): string {
+    for (let i=0; i<words.length; i++){
+        if(words[i] == words[i].split('').reverse().join('')){
+            return words[i]
+        }
+    }
+    return ''
+};
