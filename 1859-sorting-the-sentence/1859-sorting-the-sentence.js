5c7f564aefb4c8ebeeb6b8f9ca1ae8cbb078dc98
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-18 17:54:36 +0530

    Time: 44 ms (50.93%), Space: 53 MB (80.56%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -3,5 +3,16 @@
  * @return {string}
  */
 var sortSentence = function(s) {
-    return s.split(' ').sort((a,b)=> Number(a.match(/\d+/)[0]) - Number(b.match(/\d+/)[0])).join(' ').replaceAll(/\d+/g,'')
+    let words = s.split(' ')
+   let os = Array(words.length).fill('')
+
+   for(i=0;i<words.length;i++){
+       let order = words[i].at(-1)
+       let word = words[i].slice(0,words[i].length-1)
+
+       os[order-1] = word
+   }
+
+   return os.join(' ')
+
 };
