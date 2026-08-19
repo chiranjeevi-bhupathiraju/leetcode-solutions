a7ff2d8d7f6372fa864d4cd900d25c512cc9a9ff
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-19 09:14:25 +0530

    Time: 48 ms (59.93%), Space: 57.5 MB (76.95%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.ts
@@ -0,0 +1,13 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+    let res = []
+
+    for(let i=0; i< arr.length; i+=size){
+        let ch = arr.slice(i,i+size)
+        res.push(ch)
+    }
+
+    return res
+};
