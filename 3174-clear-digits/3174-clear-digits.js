33d9fb6634dc64db643ce37d02b8d33c5e8801ae
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-20 11:01:50 +0530

    Time: 2 ms (62.86%), Space: 56.7 MB (37.14%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var clearDigits = function(s) {
+    let r = ''
+
+    for(let i=0; i<s.length; i++){
+        if(!isNaN(+s[i])){
+            r = r.slice(0,r.length-1)
+        }
+        else{
+            r += s[i]
+        }
+    }
+
+    return r
+};
