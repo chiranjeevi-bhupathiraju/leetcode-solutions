8a1006322c85423e0ad461e346ac0762de59a73e
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-24 12:46:49 +0530

    Time: 3 ms (69.81%), Space: 57.1 MB (73.58%) - LeetHub

--- 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.js
+++ 2744-find-maximum-number-of-string-pairs/2744-find-maximum-number-of-string-pairs.js
@@ -5,15 +5,19 @@
 var maximumNumberOfStringPairs = function(words) {
     let ct = 0
 
-    for(let i=0; i<words.length; i++){
-        for(let j=i+1; j<words.length; j++){
+    let s = new Set()
+
+
+        for(let j=0; j<words.length; j++){
             let re = words[j].split('').reverse().join('')
 
-            if(words[i] == re){
+            if(s.has(re)){
                 ct ++
             }
+
+            s.add(words[j])
         }
-    }
+
 
     return ct
 };
