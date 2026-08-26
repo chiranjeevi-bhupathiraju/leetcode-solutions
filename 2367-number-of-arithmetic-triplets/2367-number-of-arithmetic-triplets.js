670629b9c013f91f3fe76bbb2c5ba4e6675cda95
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-26 09:49:32 +0530

    Time: 23 ms (20.39%), Space: 53.9 MB (65.05%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function (nums, diff) {
+    let ct = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) {
+                    ct++
+                }
+            }
+        }
+    }
+
+    return ct
+};
