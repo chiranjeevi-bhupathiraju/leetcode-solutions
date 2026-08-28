8ea529b264b3cbe0e93bf71f3773fdf6a090fa05
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-28 08:22:47 +0530

    Time: 0 ms (100%), Space: 55.3 MB (59%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    function dfs(n, a) {
+        if (!n) return
+
+        dfs(n.left, a)
+        a.push(n.val)
+        dfs(n.right, a)
+    }
+
+    let res = []
+
+    dfs(root, res)
+
+    return res
+};
