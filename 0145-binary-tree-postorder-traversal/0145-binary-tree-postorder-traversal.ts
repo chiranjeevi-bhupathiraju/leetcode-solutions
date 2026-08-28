c8200d339cd091245efacc12746aca31c8743737
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-28 08:13:09 +0530

    Time: 0 ms (100%), Space: 55.6 MB (39.6%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+
+    function dfs(n, a) {
+        if (!n) return
+
+        dfs(n.left, a)
+        dfs(n.right, a)
+        a.push(n.val)
+    }
+    let res = []
+    dfs(root, res)
+
+    return res
+};
