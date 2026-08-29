f97c80154539fa105db65596305a80dbe827187f
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-29 12:13:37 +0530

    Time: 0 ms (100%), Space: 58.1 MB (20.29%) - LeetHub

---
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.ts
@@ -0,0 +1,15 @@
+function mirrorDistance(n: number): number {
+    return Math.abs(n - reverseDigits(n))
+};
+
+function reverseDigits(n) {
+    let rev = 0
+
+    while (n > 0) {
+        let lastDigit = n % 10
+        rev = rev * 10 + lastDigit
+        n = Math.trunc(n / 10)
+    }
+
+    return rev
+}
