15dea7c6582cf941149ccdf2dbe3acc4c07f1f8d
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-08-30 18:01:31 +0530

    Time: 0 ms (100%), Space: 55.6 MB (65.52%) - LeetHub

---
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function increasingBST(root: TreeNode | null): TreeNode | null {
+
+    let dummyRoot = new TreeNode(0)
+    let curr = dummyRoot
+
+    function dfs(n) {
+        if (!n) return
+
+        dfs(n.left)
+
+        curr.right = new TreeNode(n.val)
+        curr = curr.right
+
+        dfs(n.right)
+    }
+
+    dfs(root)
+
+    return dummyRoot.right
+};
