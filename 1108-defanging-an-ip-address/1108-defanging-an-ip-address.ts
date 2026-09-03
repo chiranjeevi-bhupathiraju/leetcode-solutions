302b7a7a19441e702735d7c7fb2d3309970a3cbd
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-03 21:51:06 +0530

    Time: 30 ms (100%), Space: 55.7 MB (8.33%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.ts
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.ts
@@ -1,15 +1,3 @@
 function defangIPaddr(address: string): string {
-    // return address.replaceAll('.', '[.]')
-
-    let res = ''
-
-    for (let e of address) {
-        if (e == '.') {
-            res += '[.]'
-        } else {
-            res += e
-        }
-    }
-
-    return res
+    return address.replaceAll('.', '[.]')
 };
