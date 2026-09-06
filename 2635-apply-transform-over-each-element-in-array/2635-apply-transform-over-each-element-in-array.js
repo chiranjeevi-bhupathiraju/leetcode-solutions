cb396eebee25b17c8397d70fbcee8bb8f7873263
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-06 07:46:09 +0530

    Time: 37 ms (86.76%), Space: 53.7 MB (47.4%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -3,11 +3,11 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
+var map = function (arr, fn) {
     let res = []
 
-    for(i=0;i<arr.length;i++){
-        res.push(fn(arr[i],i))
+    for (i = 0; i < arr.length; i++) {
+        res.push(fn(arr[i], i))
     }
     return res
 };
