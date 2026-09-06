48a3071b39dba1219d73236ec04f8df4383ea943
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-06 07:40:57 +0530

    Time: 5 ms (79.98%), Space: 55.8 MB (92.49%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.js
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.js
@@ -2,20 +2,20 @@
  * @param {string} s
  * @return {number}
  */
-var maxFreqSum = function(s) {
+var maxFreqSum = function (s) {
     let freq = {}
-    let maxVF =0
-    let maxCF =0
+    let maxVF = 0
+    let maxCF = 0
 
-    for(let ch of s){
-      freq[ch] = (freq[ch]||0)+1
-      if('aeiou'.includes(ch)){
-         maxVF = Math.max(maxVF,freq[ch])
-      }else{
-         maxCF = Math.max(maxCF,freq[ch])
-      }
+    for (let ch of s) {
+        freq[ch] = (freq[ch] || 0) + 1
+        if ('aeiou'.includes(ch)) {
+            maxVF = Math.max(maxVF, freq[ch])
+        } else {
+            maxCF = Math.max(maxCF, freq[ch])
+        }
     }
 
-    return maxVF+maxCF
-    
+    return maxVF + maxCF
+
 };
