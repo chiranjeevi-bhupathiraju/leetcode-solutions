8eb55fbf373664d508f943fb13eeda855e7245af
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-06 07:40:21 +0530

    Time: 0 ms (100%), Space: 53.5 MB (47.92%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
@@ -2,9 +2,9 @@
  * @param {string} coordinates
  * @return {boolean}
  */
-var squareIsWhite = function(coordinates) {
+var squareIsWhite = function (coordinates) {
     if ((coordinates.charCodeAt(0) + Number(coordinates.at(1))) % 2 === 0) {
-        return false; 
+        return false;
     }
     return true;
 };
