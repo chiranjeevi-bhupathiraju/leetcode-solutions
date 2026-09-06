d2ae246248caa7552de8285242d52217c9085d7c
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-06 07:46:59 +0530

    Time: 43 ms (61.46%), Space: 55.9 MB (10.02%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.ts
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.ts
@@ -3,8 +3,8 @@ type Fn = (n: number, i: number) => any
 function filter(arr: number[], fn: Fn): number[] {
     let res = []
 
-    for(let i=0; i<arr.length; i++){
-        if(fn(arr[i],i)){
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
             res.push(arr[i])
         }
     }
