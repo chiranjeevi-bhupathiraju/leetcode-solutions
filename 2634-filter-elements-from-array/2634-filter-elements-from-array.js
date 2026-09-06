858bb92513100c9d5708fc0c2523fc8d93cf659b
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-06 07:46:49 +0530

    Time: 43 ms (58.61%), Space: 53.7 MB (39.06%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -3,11 +3,11 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function(arr, fn) {
+var filter = function (arr, fn) {
     let res = []
 
-    for(let i=0;i<arr.length;i++){
-        if(fn(arr[i],i)){
+    for (i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
             res.push(arr[i])
         }
     }
