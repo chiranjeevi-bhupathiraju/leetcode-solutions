f2914d45e1e3af80155ff21030041ab20be19aab
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-06 07:46:44 +0530

    Time: 53 ms (21.95%), Space: 54.7 MB (90.24%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
@@ -3,8 +3,8 @@ type Fn = (accum: number, curr: number) => number
 function reduce(nums: number[], fn: Fn, init: number): number {
     let res = init
 
-    for(let i=0; i<nums.length; i++){
-        res = fn(res,nums[i])
+    for (let i = 0; i < nums.length; i++) {
+        res = fn(res, nums[i])
     }
 
     return res
