be81ff471f71c1075f6b7059fd884c5d252c173a
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-06 07:46:28 +0530

    Time: 38 ms (88.76%), Space: 54.2 MB (47.92%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -4,13 +4,13 @@
  * @param {number} init
  * @return {number}
  */
-var reduce = function(nums, fn, init) {
+var reduce = function (nums, fn, init) {
     let val = init
 
-    
-    for(let i=0;i<nums.length;i++){
-       val = fn(val,nums[i])
-       
+
+    for (let i = 0; i < nums.length; i++) {
+        val = fn(val, nums[i])
+
     }
     return val
 };
