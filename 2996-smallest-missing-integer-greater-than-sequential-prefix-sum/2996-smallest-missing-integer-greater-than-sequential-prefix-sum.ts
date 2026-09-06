6c1ba2d3addffb64255f8ac40f2c2f4fe96632ed
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-06 07:47:46 +0530

    Time: 1 ms (59.22%), Space: 58 MB (21.63%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.ts
@@ -0,0 +1,22 @@
+function missingInteger(a: number[]): number {
+    let n = a.length
+    let set = new Set(a)
+    let s = a[0]
+
+    for (let i = 1; i < n; i++) {
+        let curr = a[i]
+        let prev = a[i - 1]
+        if (prev + 1 === curr) {
+            // seq
+            s += curr
+        } else {
+            break
+        }
+    }
+
+    for (; ; s++) {
+        if (!set.has(s)) {
+            return s
+        }
+    }
+};
