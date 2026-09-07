fbb7aced8bda35ca1f85f47587096092a4b07f99
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-07 12:32:05 +0530

    Time: 1 ms (51.97%), Space: 55.3 MB (8.08%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var numIdenticalPairs = function(nums) {
+var numIdenticalPairs = function (nums) {
     let ct = 0
-    for(i=0;i<nums.length;i++){
-        for(j=i+1;j<nums.length;j++){
-        if(nums[i]==nums[j]){
-            ct++
+    for (i = 0; i < nums.length; i++) {
+        for (j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                ct++
+            }
         }
-    } 
     }
     return ct
 };
