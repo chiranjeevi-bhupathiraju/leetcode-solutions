f79e5cbbf0d144a9bba9e60bf84e432ff45cfa32
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-07 12:32:31 +0530

    Time: 9 ms (17.21%), Space: 60.3 MB (22.13%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.js
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.js
@@ -3,24 +3,24 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var findIntersectionValues = function(nums1, nums2) {
-    
+var findIntersectionValues = function (nums1, nums2) {
+
     let n1s = new Set(nums1)
     let n2s = new Set(nums2)
 
     let i = 0
-    for(let e of nums1){
-        if(n2s.has(e)){
-           i++
+    for (let e of nums1) {
+        if (n2s.has(e)) {
+            i++
         }
     }
 
-     let j = 0
-    for(let e of nums2){
-        if(n1s.has(e)){
-           j++
+    let j = 0
+    for (let e of nums2) {
+        if (n1s.has(e)) {
+            j++
         }
     }
 
-   return [i,j]
+    return [i, j]
 };
