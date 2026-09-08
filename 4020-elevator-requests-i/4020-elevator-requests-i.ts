9841f340e5ea67748db17b534373624e85cef4f5
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-08 11:39:42 +0530

    Time: 1 ms (61.29%), Space: 57.3 MB (69.89%) - LeetHub

---
+++ 4020-elevator-requests-i/4020-elevator-requests-i.ts
@@ -0,0 +1,10 @@
+function elevatorRequests(n: number, requests: number[]): number {
+    requests.unshift(0)
+    let res = 0
+
+    for (let i = 1; i < requests.length; i++) {
+        res += Math.abs(requests[i] - requests[i - 1])
+    }
+
+    return res
+};
