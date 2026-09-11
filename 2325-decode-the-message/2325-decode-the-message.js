a9411243390862eaa24758d76d177b9922e5467e
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-11 11:29:25 +0530

    Time: 6 ms (51.49%), Space: 56 MB (71.29%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.js
+++ 2325-decode-the-message/2325-decode-the-message.js
@@ -3,17 +3,17 @@
  * @param {string} message
  * @return {string}
  */
-var decodeMessage = function(key, message) {
-    let keys = [...new Set(key.replaceAll(' ',''))]
+var decodeMessage = function (key, message) {
+    let keys = [...new Set(key.replaceAll(' ', ''))]
 
     let pairs = {}
 
-    for(let i =0;i<keys.length;i++){
-         pairs[keys[i]] = String.fromCharCode(i+97)
+    for (let i = 0; i < keys.length; i++) {
+        pairs[keys[i]] = String.fromCharCode(i + 97)
     }
 
     let res = ''
-    for(i=0;i<message.length;i++){
+    for (i = 0; i < message.length; i++) {
         res += pairs[message[i]] ?? ' '
     }
 
