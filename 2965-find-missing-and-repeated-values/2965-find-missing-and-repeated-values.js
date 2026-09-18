54d6fd665f5acfdebdc5dc1f3d4a8bd0d6338d0d
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-18 16:26:10 +0530

    Time: 12 ms (22.92%), Space: 62 MB (9.38%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.js
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.js
@@ -2,20 +2,20 @@
  * @param {number[][]} grid
  * @return {number[]}
  */
-var findMissingAndRepeatedValues = function(grid) {
+var findMissingAndRepeatedValues = function (grid) {
     let n = grid[0].length
     let fl = grid.flat()
 
     let set = new Set(fl)
-      let f ={}
-    for(let el of fl){
+    let f = {}
+    for (let el of fl) {
         f[el] = (f[el] || 0) + 1
     }
 
 
- let r =[]
-    for(let el in f){
-        if(+f[el] == 2){
+    let r = []
+    for (let el in f) {
+        if (+f[el] == 2) {
             r.push(+el)
             break
         }
@@ -23,13 +23,13 @@ var findMissingAndRepeatedValues = function(grid) {
 
     let i = 1
 
-    while(i<= (n*n)){
-       if(!set.has(i)){
-        r.push(i)
-        break
-       }
-       i++
+    while (i <= (n * n)) {
+        if (!set.has(i)) {
+            r.push(i)
+            break
+        }
+        i++
     }
 
-return r
+    return r
 };
