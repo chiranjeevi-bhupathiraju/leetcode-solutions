99f6de01f9807cbd5064f204a8aa115ef3ba6756
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-18 16:28:10 +0530

    Time: 0 ms (100%), Space: 53.8 MB (71.93%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -3,11 +3,11 @@
  * @param {string} word
  * @return {number}
  */
-var numOfStrings = function(patterns, word) {
+var numOfStrings = function (patterns, word) {
 
     let ct = 0
-    for(let e of patterns){
-        if(word.includes(e)){
+    for (let e of patterns) {
+        if (word.includes(e)) {
             ct++
         }
     }
