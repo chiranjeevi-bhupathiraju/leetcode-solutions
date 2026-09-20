750b4c5ad9f4ec30c142d838062f696c92325e28
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-20 18:32:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.ts
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.ts
@@ -1,17 +1,8 @@
-function countSymmetricIntegers(low: number, high: number): number {
-    let i = low
-    let ct =0
-    while(i <= high){
-        let si = String(i)
-       if(si.length % 2 == 0){
-        let fs = si.slice(0,si.length/2).split('').reduce((a,c)=>a+(+c),0)
-        let se = si.slice(si.length/2).split('').reduce((a,c)=>a+(+c),0)
-
-        if(fs == se){
-            ct++
-        }
-       }
-       i++
-    }
-    return ct
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countSymmetricIntegers = function(low, high) {
+    
 };
