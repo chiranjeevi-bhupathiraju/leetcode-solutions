55c0f1246afa9b720cd5b2a14e5af6becc0946b7
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-21 20:09:03 +0530

    Time: 3 ms (15.25%), Space: 58.8 MB (5.08%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.ts
+++ 1528-shuffle-string/1528-shuffle-string.ts
@@ -1,17 +1,17 @@
 function restoreString(s: string, indices: number[]): string {
     let mapping = {}
 
-    for(let i=0; i<indices.length; i++){
+    for (let i = 0; i < indices.length; i++) {
         let ch = s.charAt(i)
         let ind = indices[i]
 
         mapping[ind] = ch
     }
 
-    indices.sort((a,b)=>a-b)
+    indices.sort((a, b) => a - b)
     let r = ''
 
-    for(let ind of indices){
+    for (let ind of indices) {
         r += mapping[ind]
     }
 
