6e063ddf87b0f0635df19521791d583512fd4c95
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-21 20:08:25 +0530

    Time: 3 ms (31.25%), Space: 56.2 MB (50%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.ts
+++ 2315-count-asterisks/2315-count-asterisks.ts
@@ -2,12 +2,12 @@ function countAsterisks(s: string): number {
     let bc = 0
     let ac = 0
 
-    for(let i=0; i<s.length; i++){
-        if(s[i] == '|'){
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '|') {
             bc++
         }
 
-        if(bc%2== 0 && s[i] == '*'){
+        if (bc % 2 == 0 && s[i] == '*') {
             ac++
         }
     }
