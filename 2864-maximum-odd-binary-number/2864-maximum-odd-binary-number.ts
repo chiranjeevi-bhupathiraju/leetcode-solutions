f1a390d6b67ee9d549f3f9f502bc7d49d4e5abd9
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-21 20:08:39 +0530

    Time: 1 ms (78.57%), Space: 58 MB (42.86%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.ts
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.ts
@@ -1,7 +1,7 @@
 function maximumOddBinaryNumber(s: string): string {
-   const ones = s.match(/1/g).length || 0
-   const zeros = s.length-ones
+    const ones = s.match(/1/g).length || 0
+    const zeros = s.length - ones
 
 
-    return "1".repeat(ones-1)+'0'.repeat(zeros)+"1"
+    return "1".repeat(ones - 1) + '0'.repeat(zeros) + "1"
 };
