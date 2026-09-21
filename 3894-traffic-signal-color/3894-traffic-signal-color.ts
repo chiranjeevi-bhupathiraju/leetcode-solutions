a98209b82c847f0092d5f2ffff4aad4e1b1413d6
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-21 20:08:51 +0530

    Time: 0 ms (100%), Space: 58.1 MB (8.82%) - LeetHub

--- 3894-traffic-signal-color/3894-traffic-signal-color.ts
+++ 3894-traffic-signal-color/3894-traffic-signal-color.ts
@@ -1,8 +1,8 @@
 function trafficSignal(timer: number): string {
-    if(timer === 0){ return 'Green'}
-    else if(timer === 30 ){return 'Orange'}
-    else if(timer > 30 && timer <=90 ){return 'Red'}
-    else{
-      return  'Invalid'
+    if (timer === 0) { return 'Green' }
+    else if (timer === 30) { return 'Orange' }
+    else if (timer > 30 && timer <= 90) { return 'Red' }
+    else {
+        return 'Invalid'
     }
 };
