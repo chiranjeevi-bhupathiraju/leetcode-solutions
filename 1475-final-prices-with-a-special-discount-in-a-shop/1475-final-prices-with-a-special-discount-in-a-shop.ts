273a041322904ba591a43c31d7b7342e66baf593
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-22 18:45:35 +0530

    Time: 0 ms (100%), Space: 58.6 MB (42.48%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.ts
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.ts
@@ -2,14 +2,14 @@ function finalPrices(prices: number[]): number[] {
     const n = prices.length
     let res = []
 
-    for(let i=0; i<n; i++){
+    for (let i = 0; i < n; i++) {
         let price = prices[i]
-        for (let j=i+1; j<n; j++){
-            if(
+        for (let j = i + 1; j < n; j++) {
+            if (
                 prices[j] <= prices[i]
-            ){
+            ) {
 
-                price = prices[i]-prices[j]
+                price = prices[i] - prices[j]
                 break
             }
         }
