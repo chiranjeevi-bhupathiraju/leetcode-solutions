a1c8617f5f6a83818d7b928239aeb11cc2497245
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-22 18:48:55 +0530

    Time: 49 ms (22.58%), Space: 55.3 MB (32.26%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.ts
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.ts
@@ -3,12 +3,12 @@ function sortSentence(s: string): string {
     let n = words.length
     let res = Array(n).fill('')
 
-  for(let i=0; i<n; i++){
-       let order = words[i].at(-1)
-       let word = words[i].slice(0,words[i].length -1)
+    for (let i = 0; i < n; i++) {
+        let order = words[i].at(-1)
+        let word = words[i].slice(0, words[i].length - 1)
 
-       res[(+order)-1] = word
-  }
+        res[(+order) - 1] = word
+    }
 
-  return res.join(' ')
+    return res.join(' ')
 };
