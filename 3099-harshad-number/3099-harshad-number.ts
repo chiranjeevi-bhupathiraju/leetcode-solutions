9f0b4e2fd929820b663a46aa72816a72aaaa9457
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-22 18:49:01 +0530

    Time: 0 ms (100%), Space: 54.6 MB (67.86%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.ts
+++ 3099-harshad-number/3099-harshad-number.ts
@@ -1,10 +1,10 @@
 function sumOfTheDigitsOfHarshadNumber(x: number): number {
 
-    let sum = String(x).split('').reduce((a,c)=>a+ (+c),0)
+    let sum = String(x).split('').reduce((a, c) => a + (+c), 0)
 
-    if (x%sum == 0){
-       return sum
-    }else{
+    if (x % sum == 0) {
+        return sum
+    } else {
         return -1
     }
 
