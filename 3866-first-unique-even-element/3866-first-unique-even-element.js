5ecc2f136f28b6c1257a18c30131bde81ea84e2c
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-24 19:12:54 +0530

    Time: 5 ms (23.29%), Space: 56.8 MB (57.53%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.js
+++ 3866-first-unique-even-element/3866-first-unique-even-element.js
@@ -2,15 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstUniqueEven = function(nums) {
+var firstUniqueEven = function (nums) {
     let f = {}
 
-    for(let e of nums){
-        f[e] = (f[e]??0)+1
+    for (let e of nums) {
+        f[e] = (f[e] ?? 0) + 1
     }
 
-    for(let ch of nums){
-        if(f[ch] == 1 && ch%2 == 0){
+    for (let ch of nums) {
+        if (f[ch] == 1 && ch % 2 == 0) {
             return +ch
         }
     }
