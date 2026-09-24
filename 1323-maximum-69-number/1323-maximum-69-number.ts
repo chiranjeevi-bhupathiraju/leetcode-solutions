672b5b9d8d1e014ab2905abfe76481064aa2cd2a
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-24 19:29:13 +0530

    Time: 0 ms (100%), Space: 54.6 MB (76.32%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.ts
+++ 1323-maximum-69-number/1323-maximum-69-number.ts
@@ -1,14 +1,14 @@
-function maximum69Number (num: number): number {
+function maximum69Number(num: number): number {
     let maxV = num
     let s = String(num).split('')
 
-    for(let i=0; i<s.length; i++){
-        if(s[i] == '6'){
-          let  r = s.toSpliced(i,1,'9').join('')
-            maxV = Math.max(maxV,+r)
-        }else{
-          let  r = s.toSpliced(i,1,'6').join('')
-             maxV = Math.max(maxV,+r)
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == '6') {
+            let r = s.toSpliced(i, 1, '9').join('')
+            maxV = Math.max(maxV, +r)
+        } else {
+            let r = s.toSpliced(i, 1, '6').join('')
+            maxV = Math.max(maxV, +r)
         }
     }
 
