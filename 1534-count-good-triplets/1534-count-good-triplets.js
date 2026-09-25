2c55224240275b5a7e76ec2633c0951cfd5e56f4
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-25 19:32:57 +0530

    Time: 56 ms (28.74%), Space: 54.1 MB (59.77%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.js
+++ 1534-count-good-triplets/1534-count-good-triplets.js
@@ -6,14 +6,11 @@
  * @return {number}
  */
 var countGoodTriplets = function (arr, a, b, c) {
-    const n = arr.length
-
     let ct = 0
-
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             for (let k = j + 1; k < arr.length; k++) {
-                if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+                if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[k] - arr[i]) <= c) {
                     ct++
                 }
             }
