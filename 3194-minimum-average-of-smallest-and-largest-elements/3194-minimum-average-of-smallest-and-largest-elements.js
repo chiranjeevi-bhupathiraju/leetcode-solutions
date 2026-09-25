2bec8dfa6936c2195c2626c5a5de49ce5a0f174c
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-25 19:21:00 +0530

    Time: 2 ms (70.87%), Space: 55.1 MB (91.26%) - LeetHub

--- 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.js
+++ 3194-minimum-average-of-smallest-and-largest-elements/3194-minimum-average-of-smallest-and-largest-elements.js
@@ -2,16 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minimumAverage = function(nums) {
-    nums.sort((a,b)=>a-b)
+var minimumAverage = function (nums) {
+    nums.sort((a, b) => a - b)
 
     let minAvg = Infinity
-    
+
     let i = 1
-    while(i <= nums.length/2){
-        let sum = (nums.shift() + nums.pop())/2
-        minAvg = Math.min(minAvg,sum)
+    while (i <= nums.length / 2) {
+        let sum = (nums.shift() + nums.pop()) / 2
+        minAvg = Math.min(minAvg, sum)
 
     }
-     return minAvg
+    return minAvg
 };
