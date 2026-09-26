648acdc786b0945069a95c9963c32edad4a79ee6
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-26 23:38:37 +0530

    Time: 0 ms (100%), Space: 55.1 MB (91.43%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.js
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.js
@@ -2,10 +2,10 @@
  * @param {string} s
  * @return {string}
  */
-var trimTrailingVowels = function(s) {
-    for(let i=s.length-1;i>=0;i--){
-        if(!'aeiou'.includes(s[i])){
-            return s.slice(0,i+1)
+var trimTrailingVowels = function (s) {
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (!'aeiou'.includes(s[i])) {
+            return s.slice(0, i + 1)
         }
     }
     return ''
