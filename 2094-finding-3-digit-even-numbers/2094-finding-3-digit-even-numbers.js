ea3b596db3ac63d5d9d3c2ccdc065bd4a3a092d9
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-26 23:38:25 +0530

    Time: 2150 ms (5.62%), Space: 58 MB (65.17%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.js
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.js
@@ -2,28 +2,28 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var findEvenNumbers = function(digits) {
+var findEvenNumbers = function (digits) {
     let res = []
 
-    for(i=0;i<digits.length;i++){
-        let di =digits[i]
-        if(di == 0) continue
-        for(j=0;j<digits.length;j++){
-            if(i === j) continue
+    for (i = 0; i < digits.length; i++) {
+        let di = digits[i]
+        if (di == 0) continue
+        for (j = 0; j < digits.length; j++) {
+            if (i === j) continue
             let dj = digits[j]
-        for(k=0;k<digits.length;k++){
-            let dk = digits[k]
-              if(j === k || k === i || dk % 2 !== 0) continue
-            let num = 100 * di + 10 * dj + 1 * dk
+            for (k = 0; k < digits.length; k++) {
+                let dk = digits[k]
+                if (j === k || k === i || dk % 2 !== 0) continue
+                let num = 100 * di + 10 * dj + 1 * dk
 
-            if(res.indexOf(num) === -1){
-                res.push(num)
+                if (res.indexOf(num) === -1) {
+                    res.push(num)
+
+                }
 
             }
-              
-    }
-    }
+        }
     }
 
-    return res.sort((a,b)=>a-b)
+    return res.sort((a, b) => a - b)
 };
