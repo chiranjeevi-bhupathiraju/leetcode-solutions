51208d25d56bce532eff87bd8121bf26217db9ad
Chiranjeevi Bhupathiraju <chiranjeevi.bhupathiraju.lead@gmail.com>
2026-09-26 23:38:32 +0530

    Time: 2 ms (59.96%), Space: 61 MB (51.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.ts
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -3,17 +3,17 @@
  */
 function moveZeroes(nums: number[]): void {
 
-let k =0
-  for(let i=0;i<nums.length;i++){
-    if(nums[i] !== 0){
-        nums[k] = nums[i]
+    let k = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[k] = nums[i]
+            k++
+        }
+    }
+
+    while (k < nums.length) {
+        nums[k] = 0
         k++
     }
-  }
-
-  while(k<nums.length){
-    nums[k] = 0
-    k++
-  }
 
 };
